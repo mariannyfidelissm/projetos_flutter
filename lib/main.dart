2e350628e3e3667dad9eb5574b2a9d9b7a83a96e
Marianny Fidelis <marianny.fidelis@ifce.edu.br>
2025-03-21 11:37:45 -0300

    Configurando a main assíncrona

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:primeiro_app_flutter/06_bytebank/database/app_database.dart';
 import 'package:primeiro_app_flutter/06_bytebank/models/contact.dart';
 import 'package:primeiro_app_flutter/07_notas_diarias/models/journal.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '01_banco_douro/banco_douro_app.dart';
 import '01_banco_douro/login_page.dart';
 import '02_app_cinetopia/app/app.dart';
@@ -11,7 +12,7 @@ import '05_my_todo_list_/todolist_gamification.dart';
 import '07_notas_diarias/note_diary_app.dart';
 import '07_notas_diarias/service/journal_service.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   //runApp(BytebankApp());
@@ -21,9 +22,23 @@ void main() {
   //runApp(BytebankApp());
   //runApp(const TodoListGamification());
   {
-    runApp(const NoteDiaryApp());
+    bool isLogged = await verifyToken();
+    runApp(NoteDiaryApp(
+      isLogged: isLogged,
+    ));
+
     JournalService journalService = JournalService();
     //journalService.register(Journal.empty());
-    journalService.getAll();
+    //journalService.getAll();
+  }
+}
+
+Future<bool> verifyToken() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? token = prefs.getString("accessToken");
+  if (token != null) {
+    return true;
+  } else {
+    return false;
   }
 }
