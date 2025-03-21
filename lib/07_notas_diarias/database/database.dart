9d85e338aff8fb74a76701a11310354d6d6ce160
Marianny Fidelis <marianny.fidelis@ifce.edu.br>
2025-03-21 17:58:58 -0300

    Comentando o database fixo pois agora usaremos web API com JSON-SERVER-AUTH

--- lib/07_notas_diarias/database/database.dart
+++ lib/07_notas_diarias/database/database.dart
@@ -1,30 +1,30 @@
-import 'dart:math';
-import 'package:uuid/uuid.dart';
-import '../helpers/phrases.dart';
-import '../models/journal.dart';
-
-Map<String, Journal> generateRandomDatabase({
-  required int maxGap, // Tamanho máximo da janela de tempo
-  required int amount, // Entradas geradas
-}) {
-  Random rng = Random();
-
-  Map<String, Journal> map = {};
-
-  for (int i = 0; i < amount; i++) {
-    int timeGap = rng.nextInt(maxGap - 1); // Define uma distância do hoje
-    DateTime date = DateTime.now().subtract(
-      Duration(days: timeGap),
-    ); // Gera um dia
-
-    String id = const Uuid().v1();
-
-    map[id] = Journal(
-      id: id,
-      content: getRandomPhrase(),
-      createdAt: date,
-      updatedAt: date,
-    );
-  }
-  return map;
-}
+// import 'dart:math';
+// import 'package:uuid/uuid.dart';
+// import '../helpers/phrases.dart';
+// import '../models/journal.dart';
+//
+// Map<String, Journal> generateRandomDatabase({
+//   required int maxGap, // Tamanho máximo da janela de tempo
+//   required int amount, // Entradas geradas
+// }) {
+//   Random rng = Random();
+//
+//   Map<String, Journal> map = {};
+//
+//   for (int i = 0; i < amount; i++) {
+//     int timeGap = rng.nextInt(maxGap - 1); // Define uma distância do hoje
+//     DateTime date = DateTime.now().subtract(
+//       Duration(days: timeGap),
+//     ); // Gera um dia
+//
+//     String id = const Uuid().v1();
+//
+//     map[id] = Journal(
+//       id: id,
+//       content: getRandomPhrase(),
+//       createdAt: date,
+//       updatedAt: date,
+//     );
+//   }
+//   return map;
+// }
